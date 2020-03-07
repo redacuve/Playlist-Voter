6bad74272b14f452e9117e3f13d5ea22e6c17d6a
Rey David Cuevas <cuevas.vela@gmail.com>
2020-03-07 17:47:39 -0600

    added validation ton the models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -1,2 +1,5 @@
 class Playlist < ApplicationRecord
+    has_many :track
+    validates :votes, presence: true
+    validates :name, presence: true
 end
--- app/models/track.rb
+++ app/models/track.rb
@@ -1,2 +1,6 @@
 class Track < ApplicationRecord
+    belongs_to :playlist
+    validates :name, presence: true
+    validates :title, presence: true
+    validates :artist, presence: true
 end
